207fa04f97a0a8f0ff14d92fff8140fdbcb2b2b4
FabsHC <fabio.hc94@gmail.com>
2024-10-20 19:24:27 -0300

    feat: Unit tests with gomonkey to force fail in function

--- cmd/app/handlers/terminal_handler_test.go
+++ cmd/app/handlers/terminal_handler_test.go
@@ -5,11 +5,19 @@ import (
 	"capital-gain/internal/config"
 	"capital-gain/internal/models"
 	"encoding/json"
+	"errors"
+	"github.com/agiledragon/gomonkey/v2"
+	_ "github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"os"
 	"strings"
 	"testing"
+)
+
+var (
+	validInput   = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100},{\"operation\":\"sell\", \"unit-cost\":10.00, \"quantity\": 100}]"
+	invalidInput = "[{]"
 )
 
 func TestHandler(t *testing.T) {
@@ -18,8 +26,7 @@ func TestHandler(t *testing.T) {
 
 	t.Run("should execute with success with case_1 data", func(t *testing.T) {
 		handler := handlers.NewTerminalHandler(reg.BuyOperation, reg.SellOperation, reg.TaxCalculation)
-		input := "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100},{\"operation\":\"sell\", \"unit-cost\":10.00, \"quantity\": 100}]"
-		var inputReader io.Reader = strings.NewReader(input)
+		var inputReader io.Reader = strings.NewReader(validInput)
 
 		reader, writer, err := os.Pipe()
 		assert.Nil(t, err)
@@ -40,6 +47,54 @@ func TestHandler(t *testing.T) {
 		assert.Equal(t, 2, len(capitalGainOutputs))
 		assert.Equal(t, 0.0, *capitalGainOutputs[0].Tax)
 		assert.Equal(t, 0.0, *capitalGainOutputs[1].Tax)
+
+		os.Stdout = originalStdout
+	})
+
+	t.Run("should execute with failure with invalid validInput data", func(t *testing.T) {
+		handler := handlers.NewTerminalHandler(reg.BuyOperation, reg.SellOperation, reg.TaxCalculation)
+
+		var inputReader io.Reader = strings.NewReader(invalidInput)
+
+		reader, writer, err := os.Pipe()
+		assert.Nil(t, err)
+		originalStdout := os.Stdout
+		os.Stdout = writer
+
+		handler.Execute(inputReader)
+		err = writer.Close()
+		assert.Nil(t, err)
+
+		out, err := io.ReadAll(reader)
+		assert.Nil(t, err)
+
+		assert.Empty(t, out)
+
+		os.Stdout = originalStdout
+	})
+
+	t.Run("should execute with failure, json encode error", func(t *testing.T) {
+		gomonkey.ApplyFunc((*json.Encoder).Encode, func(_ interface{}) error {
+			return errors.New("fail")
+		})
+
+		handler := handlers.NewTerminalHandler(reg.BuyOperation, reg.SellOperation, reg.TaxCalculation)
+
+		var inputReader io.Reader = strings.NewReader(validInput)
+
+		reader, writer, err := os.Pipe()
+		assert.Nil(t, err)
+		originalStdout := os.Stdout
+		os.Stdout = writer
+
+		handler.Execute(inputReader)
+		err = writer.Close()
+		assert.Nil(t, err)
+
+		out, err := io.ReadAll(reader)
+		assert.Nil(t, err)
+
+		assert.Empty(t, out)
 
 		os.Stdout = originalStdout
 	})
