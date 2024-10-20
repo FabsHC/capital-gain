2af0c2667877eb2e83faa987f7a8180007029479
FabsHC <fabio.hc94@gmail.com>
2024-10-20 17:49:09 -0300

    feat: add unit tests

---
+++ internal/services/buy_operation_test.go
@@ -0,0 +1,30 @@
+package services_test
+
+import (
+	"capital-gain/internal/models"
+	"capital-gain/internal/services"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBuyOperation(t *testing.T) {
+	t.Parallel()
+	buyOperationService := services.NewBuyOperation()
+	purchase := models.NewPurchase()
+
+	t.Run("should execute buy operation and set average price to 10 on first execution", func(t *testing.T) {
+		operation := models.NewCapitalGainInput(models.BUY_OPERATION, 10, 100)
+		buyOperationService.Execute(purchase, *operation)
+
+		assert.Equal(t, float64(10), purchase.AveragePrice)
+		assert.Equal(t, operation.Quantity, purchase.Stocks)
+	})
+
+	t.Run("should execute buy operation and set average price to 7.5 and 200 total stocks on second execution", func(t *testing.T) {
+		operation := models.NewCapitalGainInput(models.BUY_OPERATION, 5, 100)
+		buyOperationService.Execute(purchase, *operation)
+
+		assert.Equal(t, 7.5, purchase.AveragePrice)
+		assert.Equal(t, uint(200), purchase.Stocks)
+	})
+}
---
+++ internal/services/sell_operation_test.go
@@ -0,0 +1,56 @@
+package services_test
+
+import (
+	"capital-gain/internal/models"
+	"capital-gain/internal/services"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSellOperation(t *testing.T) {
+	t.Parallel()
+	buyOperation := services.NewBuyOperation()
+	sellOperation := services.NewSellOperation()
+
+	t.Run("should execute sell operation and do not have any gains/losses because average price is same as selling price", func(t *testing.T) {
+		purchase := models.NewPurchase()
+		sale := models.NewSale()
+		operation := models.NewCapitalGainInput(models.BUY_OPERATION, 10, 100)
+
+		buyOperation.Execute(purchase, *operation)
+		assert.Equal(t, float64(10), purchase.AveragePrice)
+
+		operation = models.NewCapitalGainInput(models.SELL_OPERATION, 10, 100)
+		sellOperation.Execute(purchase, sale, *operation)
+		assert.Equal(t, float64(0), sale.Gains)
+		assert.Equal(t, float64(0), sale.Losses)
+	})
+
+	t.Run("should execute sell operation and have losses because selling price is bellow average price", func(t *testing.T) {
+		purchase := models.NewPurchase()
+		sale := models.NewSale()
+		operation := models.NewCapitalGainInput(models.BUY_OPERATION, 10, 100)
+
+		buyOperation.Execute(purchase, *operation)
+		assert.Equal(t, float64(10), purchase.AveragePrice)
+
+		operation = models.NewCapitalGainInput(models.SELL_OPERATION, 5, 100)
+		sellOperation.Execute(purchase, sale, *operation)
+		assert.Equal(t, float64(0), sale.Gains)
+		assert.Equal(t, float64(500), sale.Losses)
+	})
+
+	t.Run("should execute sell operation and have gains because selling price is higher than average price", func(t *testing.T) {
+		purchase := models.NewPurchase()
+		sale := models.NewSale()
+		operation := models.NewCapitalGainInput(models.BUY_OPERATION, 10, 100)
+
+		buyOperation.Execute(purchase, *operation)
+		assert.Equal(t, float64(10), purchase.AveragePrice)
+
+		operation = models.NewCapitalGainInput(models.SELL_OPERATION, 25, 100)
+		sellOperation.Execute(purchase, sale, *operation)
+		assert.Equal(t, float64(1500), sale.Gains)
+		assert.Equal(t, float64(0), sale.Losses)
+	})
+}
