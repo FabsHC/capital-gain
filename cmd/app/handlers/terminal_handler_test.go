2d988af1868b150aec384128e463651e9e89d7a0
FabsHC <fabio.hc94@gmail.com>
2024-10-20 18:43:44 -0300

    feat: add unit tests

---
+++ cmd/app/handlers/terminal_handler_test.go
@@ -0,0 +1,46 @@
+package handlers_test
+
+import (
+	"capital-gain/cmd/app/handlers"
+	"capital-gain/internal/config"
+	"capital-gain/internal/models"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	t.Parallel()
+	reg := config.NewRegister()
+	handler := handlers.NewTerminalHandler(reg.BuyOperation, reg.SellOperation, reg.TaxCalculation)
+
+	t.Run("should execute with success with case_1 data", func(t *testing.T) {
+		input := "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100},{\"operation\":\"sell\", \"unit-cost\":10.00, \"quantity\": 100}]"
+		var inputReader io.Reader = strings.NewReader(input)
+
+		reader, writer, err := os.Pipe()
+		assert.Nil(t, err)
+		os.Stdout = writer
+
+		handler.Execute(inputReader)
+		err = writer.Close()
+		assert.Nil(t, err)
+
+		out, err := io.ReadAll(reader)
+		assert.Nil(t, err)
+
+		err = os.Stdout.Close()
+		assert.Nil(t, err)
+
+		var capitalGainOutputs []models.CapitalGainOutput
+		err = json.Unmarshal(out, &capitalGainOutputs)
+		assert.Nil(t, err)
+
+		assert.Equal(t, 2, len(capitalGainOutputs))
+		assert.Equal(t, 0.0, *capitalGainOutputs[0].Tax)
+		assert.Equal(t, 0.0, *capitalGainOutputs[1].Tax)
+	})
+}
